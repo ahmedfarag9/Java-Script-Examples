92f128b4f3d4ba8f3575b2dff5d12057de980835
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-02-08 01:50:40 +0200

    added a new array array of TODO items

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
-}
+/*let classNames = {
+	TODO_ITEM: 'todo-container',
+	TODO_CHECKBOX: 'todo-checkbox',
+	TODO_TEXT: 'todo-text',
+	TODO_DELETE: 'todo-delete',
+}*/
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
-
-function newTodo() {
-  alert('New TODO button clicked!')
-}
+let todo_list = [];
