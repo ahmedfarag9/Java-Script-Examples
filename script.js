2c8a6b10d1f0cdc2a1423faf9989411ca513e962
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-02-08 01:52:11 +0200

    Get DOM elements' values from html

--- script.js
+++ script.js
@@ -6,3 +6,8 @@
 }*/
 
 let todo_list = [];
+ul = document.getElementById('todo_list');
+//let list = document.getElementById('todo-list').textContent;
+let itemCountSpan = parseInt(document.getElementById('item-count').textContent);
+let uncheckedCountSpan = parseInt(document.getElementById('unchecked-count').textContent);
+
